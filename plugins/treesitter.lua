c9e58f998d4aebcb16866372beb2642d5b5aa5d1
bad4iz <developer@bad4iz.ru>
2023-03-12 01:52:27 +0400

    rainbow

--- plugins/treesitter.lua
+++ plugins/treesitter.lua
@@ -1,6 +1,26 @@
 return {
   "nvim-treesitter/nvim-treesitter",
+  dependencies = { -- add a new dependency to telescope that is our new plugin
+    "p00f/nvim-ts-rainbow",
+  },
   opts = {
+    ensure_installed = { "javascript", "tsx" },
+    sync_install = false,
+    ignore_install = { "" }, -- List of parsers to ignore installing
+    highlight = {
+      enable = true,         -- false will disable the whole extension
+      disable = { "" },      -- list of language that will be disabled
+      additional_vim_regex_highlighting = true,
+    },
+    indent = { enable = true, disable = { "yaml" } },
+    rainbow = {
+      enable = true,
+      -- disable = { "jsx", "cpp" }, list of languages you want to disable the plugin for
+      extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
+      max_file_lines = nil, -- Do not enable for files with more than n lines, int
+      -- colors = {}, -- table of hex strings
+      -- termcolors = {} -- table of colour name strings
+    }
     -- ensure_installed = { "lua" },
   },
 }
