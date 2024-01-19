96347a04b69d17a13ad5f9109ba4f7164f849bc5
bad4iz <developer@bad4iz.ru>
2024-01-19 21:29:14 +0400

    community

--- plugins/community.lua
+++ plugins/community.lua
@@ -7,5 +7,22 @@ return {
   -- { import = "astrocommunity.colorscheme.catppuccin" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp", lazy = false },
   { import = "astrocommunity.utility.noice-nvim", lazy = false },
+  { import = "astrocommunity.editing-support.neogen", lazy = true },
+  { import = "astrocommunity.editing-support.rainbow-delimiters-nvim", lazy = false },
+  { import = "astrocommunity.editing-support.telescope-undo-nvim", lazy = false },
+  { import = "astrocommunity.editing-support.vim-move", lazy = false },
+  { import = "astrocommunity.editing-support.wildfire-nvim", lazy = false },
+  { import = "astrocommunity.motion.nvim-surround", lazy = false },
+  { import = "astrocommunity.motion.hop-nvim", lazy = false },
+  { import = "astrocommunity.pack.typescript", lazy = false },
+  { import = "astrocommunity.pack.tailwindcss", lazy = false },
+  { import = "astrocommunity.pack.vue", lazy = false },
+  { import = "astrocommunity.pack.lua", lazy = false },
+  { import = "astrocommunity.pack.json", lazy = false },
+  { import = "astrocommunity.pack.html-css", lazy = false },
+  { import = "astrocommunity.register.nvim-neoclip-lua", lazy = false },
+  { import = "astrocommunity.scrolling.mini-animate", lazy = false },
+  { import = "astrocommunity.workflow.bad-practices-nvim", lazy = false },
+  
 }
 
