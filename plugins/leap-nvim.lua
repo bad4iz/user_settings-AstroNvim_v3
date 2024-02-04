58eb5f7f655ebb7d5b1b9fd3e22e2a44211b0a8a
bad4iz <developer@bad4iz.ru>
2024-02-05 01:22:51 +0400

    d

---
+++ plugins/leap-nvim.lua
@@ -0,0 +1,35 @@
+return {
+  {
+    "ggandor/leap.nvim",
+    keys = {
+      -- { "t", "<Plug>(leap-forward-to)", mode = { "n" }, desc = "Leap forward to" },
+      -- { "T", "<Plug>(leap-backward-to)", mode = { "n" }, desc = "Leap backward to" },
+    },
+    opts = {},
+    init = function() -- https://github.com/ggandor/leap.nvim/issues/70#issuecomment-1521177534
+      vim.api.nvim_create_autocmd("User", {
+        callback = function()
+          vim.cmd.hi("Cursor", "blend=100")
+          vim.opt.guicursor:append { "a:Cursor/lCursor" }
+        end,
+        pattern = "LeapEnter",
+      })
+      vim.api.nvim_create_autocmd("User", {
+        callback = function()
+          vim.cmd.hi("Cursor", "blend=0")
+          vim.opt.guicursor:remove { "a:Cursor/lCursor" }
+        end,
+        pattern = "LeapLeave",
+      })
+    end,
+    dependencies = {
+      "tpope/vim-repeat",
+    },
+  },
+  {
+    "catppuccin/nvim",
+    optional = true,
+    ---@type CatppuccinOptions
+    opts = { integrations = { leap = true } },
+  },
+}
